a975c691339a7b42c50d17224554d9c6ea1dbee1
Rian Nunes <ryannunes70@gmail.com>
2022-04-13 21:25:12 -0400

    atualização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,12 @@ export class AppComponent {
     { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title:'Navegação1', url: 'Navegação1', icon: 'code'},
+    { title:'Navegação2', url: 'Navegação2', icon: 'code'},
+    { title:'Navegação3', url: 'Navegação3', icon: 'code'},
+    { title:'Navegação4', url: 'Navegação4', icon: 'code'},
+    { title:'Navegação5', url: 'Navegação5', icon: 'code'},
+
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
